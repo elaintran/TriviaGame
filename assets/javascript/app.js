ebc52ab74924f89ba1eed16ee945634ad5f42406
elaintran <elain.ttran@gmail.com>
2019-06-04 18:10:20 -0500

    appending questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,61 @@
 $(document).ready(function() {
     var questionList = [
         {
-            questionOne: "question",
+            question: "question1",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question2",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question3",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question4",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question5",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question6",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question7",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question8",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question9",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }, {
+            question: "question10",
             choices: ["a", "b", "c", "d"],
             answer: "a"
         }
     ]
-    console.log(questionList[0].choices[0]);
+
+    var questionIndex = 0;
+    var questionNumber = 1;
+
+    function createTrivia() {
+        $(".question-number").text(questionNumber);
+        $(".total-questions").text(questionList.length)
+        $(".question").text(questionList[questionIndex].question);
+        for (var i = 0; i < 4; i++) {
+            var answerChoices = questionList[questionIndex].choices[i];
+            var answerCard = $("<div>").addClass("answer-card").append("<p>" + answerChoices + "</p>");
+            $(".answer-container").append(answerCard);
+        }
+    }
+    createTrivia();
+
 })
