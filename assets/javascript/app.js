37f66e6355b6f0ff5a9487bc7e8af9cd2ccfb9da
elaintran <elain.ttran@gmail.com>
2019-06-04 17:40:59 -0500

    jQuery set up and object testing

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var questionList = [
+        {
+            questionOne: "question",
+            choices: ["a", "b", "c", "d"],
+            answer: "a"
+        }
+    ]
+    console.log(questionList[0].choices[0]);
+})
