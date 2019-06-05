2cadd354f03bd85b0560f5d8e6ce95c2e518280f
elaintran <elain.ttran@gmail.com>
2019-06-04 19:58:57 -0500

    change color according to right or wrong answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,11 +46,19 @@ $(document).ready(function() {
     var index = 0;
     var questionNumber = 1;
     var answerClicked = false;
+    var correct = 0;
+    var incorrect = 0;
 
     function createTrivia() {
+        //current question number
         $(".question-number").text(questionNumber);
+        //total question number
         $(".total-questions").text(questionList.length)
+        //current question
         $(".question").text(questionList[index].question);
+        //clear answer container
+        $(".answer-container").empty();
+        //add in answer options to answer container
         for (var i = 0; i < 4; i++) {
             var answerChoices = questionList[index].choices[i];
             var answerCard = $("<div>").addClass("answer-card").append("<p>" + answerChoices + "</p>");
@@ -59,13 +67,23 @@ $(document).ready(function() {
     }
     createTrivia();
 
+    //click on answer
     $(".answer-card").on("click", function() {
+        //picked correct answer
         if ($(this).text() === questionList[index].answer && answerClicked == false) {
-            console.log("hi");
+            checkAnswer(correct, this, "correct");
+        //picked incorrect answer
         } else if (answerClicked == false) {
-            console.log("bye");
+            checkAnswer(incorrect, this, "incorrect");
         }
+        //prevents from clicking afterwards
         answerClicked = true;
     })
 
+    function checkAnswer(answer, element, addClass) {
+        //answer counter
+        answer++;
+        //change color according to right or wrong
+        $(element).addClass(addClass);
+    }
 })
